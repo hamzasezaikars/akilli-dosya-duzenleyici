72a0e25bd17c762f44ace769647d01320a3ec8af
hamzasezaikars <hamzasezai9898@gmail.com>
2026-01-11 00:29:23 +0300

    İlk kod satırları yazıldı.

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import os
+def klasoru_tara(klasor_yolu="."):
+    print(f"Klasör taranıyor: {klasor_yolu}\n")
+    for eleman in os.listdir(klasor_yolu)
+    print(eleman)
+    if __name__=="__manin__":
+        klasoru_tara()
+        
