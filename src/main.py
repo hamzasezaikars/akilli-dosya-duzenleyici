4ad505fdb032f0fc04141dd6e174ec26f0ff93c0
hamzasezaikars <hamzasezai9898@gmail.com>
2026-01-12 00:38:15 +0300

    Güncelleme

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import os
+
 def klasoru_tara(klasor_yolu="."):
     print(f"Klasör taranıyor: {klasor_yolu}\n")
-    for eleman in os.listdir(klasor_yolu)
-    print(eleman)
-    if __name__=="__manin__":
-        klasoru_tara()
-        
+
+    for eleman in os.listdir(klasor_yolu):
+        tam_yol = os.path.join(klasor_yolu, eleman)
+
+        if os.path.isfile(tam_yol):
+            dosya_adi, uzanti = os.path.splitext(eleman)
+            print(f"Dosya: {dosya_adi} | Uzantı: {uzanti}")
+
+if __name__ == "__main__":
+    klasoru_tara()
